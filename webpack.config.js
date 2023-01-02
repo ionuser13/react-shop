c51669e3079e3701b9f36403fd3a3e5adbe9f65f
John Chacpi <johnchacpi479@gmail.com>
2023-01-01 22:22:35 -0500

    added styles files and new components

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.s[ac]ss/i,
+                test: /\.(css|scss)/i,
                 use: [
                     "style-loader",
                     "css-loader",
