c4858f68e1afaa6435dc6ebf162b3c73a402c1a5
John Chacpi <johnchacpi479@gmail.com>
2023-01-17 14:52:07 -0500

    addedd more components to the app routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../containers/Layout";
-import NewPassword from "../containers/NewPassword";
-import EmailSent from "../containers/EmailSent";
+import NewPassword from "../pages/NewPassword";
+import EmailSent from "../pages/EmailSent";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import Login from "../pages/Login";
+import Checkout from "../pages/Checkout";
+import MyAccountMobile from "../pages/MyAccountMobile";
+import Orders from "../pages/Orders";
+import PasswordRecovery from "../pages/PasswordRecovery";
+import Register from "../pages/Register";
 import "../styles/global.css";
 
 const App = () => {
     return(
         <BrowserRouter>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/new-password" element={<NewPassword />} />
-                <Route exact path="/email-sent" element={<EmailSent />} />
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
+            <Layout>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route  exact path="/login" element={<Login />} />
+                    <Route exact path="/password-recovery" element={<PasswordRecovery/>} />
+                    <Route exact path="/email-sent" element={<EmailSent />} />
+                    <Route exact path="/new-password" element={<NewPassword />} />
+                    <Route
+                    exact path="/account" element={<MyAccountMobile/>} />
+                    <Route exact path="/register" element={<Register />} />
+                    <Route exact path="/checkout" element={<Checkout />} />
+                    <Route exact path="/orders" element={<Orders />} />
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </Layout>
         </BrowserRouter>
     )
 }
