4d0e9157944bc05c652e815e003c9fc5efefa0db
John Chacpi <johnchacpi479@gmail.com>
2023-01-16 23:42:52 -0500

    add checkout and password recovery

---
+++ src/components/DesktopMenu.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "../styles/desktopMenu.scss"
+
+const DesktopMenu = () => {
+    return (
+      <div className="desktop-menu">
+        <ul>
+          <li>
+            <a href="/" className="title">
+              My orders
+            </a>
+          </li>
+
+          <li>
+            <a href="/">My account</a>
+          </li>
+
+          <li>
+            <a href="/">Sign out</a>
+          </li>
+        </ul>
+      </div>
+    );
+}
+{/*Menu*/}
+export default DesktopMenu
