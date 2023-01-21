0362241098bce342d8ec0fce89b6f965280f8e05
John Chacpi <johnchacpi479@gmail.com>
2023-01-21 10:33:16 -0500

    added images to components

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import "../styles/productInfo.scss"
+import "../styles/productInfo.scss";
+import close from "@icons/icon_close.png";
+import addToCart from "@icons/bt_add_to_cart.svg";
 
 const ProductInfo = () => {
     return (
       <aside className="product-detail">
         <div className="product-detail-close">
-          <img src="./icons/icon_close.png" alt="close" />
+          <img src={close} alt="close" />
         </div>
         <img
           src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
@@ -19,7 +21,7 @@ const ProductInfo = () => {
             function, add your hall or workspace.
           </p>
           <button className="primary-button add-to-cart-button">
-            <img src="./icons/bt_add_to_cart.svg" alt="add to cart" />
+            <img src={addToCart} alt="add to cart" />
             Add to cart
           </button>
         </div>
