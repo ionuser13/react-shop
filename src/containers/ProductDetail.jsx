c725992cf8d048abc25b955c1d973161ce44ff1c
John Chacpi <johnchacpi479@gmail.com>
2023-01-21 10:57:13 -0500

    added icons to containers

--- src/containers/ProductDetail.jsx
+++ src/containers/ProductDetail.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ProductInfo from "../components/ProductInfo";
+import close from "@icons/icon_close.png";
 import "../styles/ProductDetail.scss";
 
 const ProductDetail = () => {
     return (
         <div className="ProductDetail">
             <div className="product-detail-close">
-                <img src="./icons/icon_close.png" alt="close" />
+                <img src={close} alt="close" />
             </div>
             <ProductInfo />
         </div>
