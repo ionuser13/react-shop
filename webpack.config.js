9c8462074831ae9be6efcb603013b065775f1432
John Chacpi <johnchacpi479@gmail.com>
2023-01-26 15:54:56 -0500

    [ADD] GitHub Actions

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,8 @@ module.exports = {
             "@containers": path.resolve(__dirname, "src/containers"),
             "@pages": path.resolve(__dirname, "src/pages"),
             "@styles": path.resolve(__dirname, "src/styles"),
-            "@icons": path.resolve(__dirname, "src/assets/icons"),
-            "@logos": path.resolve(__dirname, "src/assets/logos"),
+            "@icons": path.resolve(__dirname, "src/Assets/icons"),
+            "@logos": path.resolve(__dirname, "src/Assets/logos"),
             "@hooks": path.resolve(__dirname, "src/hooks"),
             "@context": path.resolve(__dirname, "src/context")
         }
