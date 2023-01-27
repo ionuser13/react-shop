819b7f4ae37526dd4f1cb83aae04b6397f43d43d
John Chacpi <johnchacpi479@gmail.com>
2023-01-27 17:40:58 -0500

    changes on index js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./routes/App"
 
-const root = createRoot(document.getElementById("app"))
-root.render(<App />)
+ReactDOM.render(<App />, document.getElementById("app"))
